5fb265770c36fed031332d9509f4d21194b8bcde
patrikz1 <43486073+patrikz1@users.noreply.github.com>
2022-04-22 15:51:43 +0200

    .

--- Lab_3/Lab 3/TestClass/UnitTest1.cs
+++ Lab_3/Lab 3/TestClass/UnitTest1.cs
@@ -1,13 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_3;
+
 
 namespace TestClass
 {
     [TestClass]
     public class UnitTest1
     {
+        string TestData = "NW.CC, NC.CC, NW.NW, NE.CC, NW.SE, CE.CC, CW.CC, SE.CC, CW.NW, CC.CC, CW.SE, CC.NW, CC.SE, CE.NW, SW.CC, CE.SE, SW.NW, SE.SE, SW.SE";
+        string ExpectedWinner = "X";
+
         [TestMethod]
-        public void TestMethod1()
+        public void ExpectedResultTest()
         {
+            var makemoves = new Game(new GameSetup().PopulateBoards(), new GameSetup().PopulatePlayers(), TestData).MakeMoves(new GameSetup().PopulatePlayers(), new GameSetup().PopulateBoards(), TestData);
+            var result = new Result(makemoves);
+            Assert.AreEqual(ExpectedWinner, result.WinnerPlayer);
         }
     }
 }
