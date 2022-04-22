2136e4846af0f682126dd489c0a6b6f79593397d
patrikz1 <43486073+patrikz1@users.noreply.github.com>
2022-04-22 15:48:41 +0200

    .

---
+++ Lab_3/Lab 3/TestClass/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
